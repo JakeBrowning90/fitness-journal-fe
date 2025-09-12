eb7b270ea73d558482eb3c58aa6ea6e31f5e2749
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-09-11 21:35:16 -0400

    Add placeholder HomeScreen and Login components

---
+++ src/components/HomeScreen.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import { Routes, Route, Link, useNavigate, useParams } from "react-router";
+
+// import apiSource
+import { apiSource } from "../apiSource";
+
+function HomeScreen(
+  {
+    // Props
+  }
+) {
+  // State declarations
+  const [content, setContent] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // Functions
+  useEffect(() => {
+    fetch(apiSource + `opportunity`, {
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("Fetch error");
+        }
+        return response.json();
+      })
+      .then((response) => setContent(response))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+  
+  // Render
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Network error, please try again later.</p>;
+  return <></>;
+}
+
+export default HomeScreen;
