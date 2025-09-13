0a10ff3b988571d32076c42ecb2d4da4c3e53710
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-09-13 18:45:53 -0400

    Add basic fetch for sessions on home screen

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -15,30 +15,49 @@ function HomeScreen(
   const [error, setError] = useState(null);
 
   // Functions
-  // useEffect(() => {
-  //   fetch(apiSource + `opportunity`, {
-  //     mode: "cors",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //   })
-  //     .then((response) => {
-  //       if (response.status >= 400) {
-  //         throw new Error("Fetch error");
-  //       }
-  //       return response.json();
-  //     })
-  //     .then((response) => setContent(response))
-  //     .catch((error) => setError(error))
-  //     .finally(() => setLoading(false));
-  // }, []);
+  useEffect(() => {
+    fetch(apiSource + `session`, {
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("Fetch error");
+        }
+        return response.json();
+      })
+      .then((response) => {
+        setContent(response);
+        console.log(response);
+      })
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
 
   // Render
-  // if (loading) return <p>Loading...</p>;
-  // if (error) return <p>Network error, please try again later.</p>;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Network error, please try again later.</p>;
   return (
     <>
       <h1>Home Screen</h1>
+      {content.length == 0 ? (
+        <span>No sessions found</span>
+      ) : (
+        <ul>
+          {content.map((session) => {
+            return (
+              <li key={session.id}>
+                <span>{session.date}</span>
+                <span>{session.exercise[0].name}</span>
+                <span>Distance: {session.distancek}</span>
+                <span>Time: {session.durationmin}</span>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </>
   );
 }
