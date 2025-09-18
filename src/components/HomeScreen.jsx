9e6e55524a9b3b521f736c66e9c111047e4e2a5b
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-09-17 20:38:59 -0400

    Add states and handlers to session form

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -11,6 +11,12 @@ function HomeScreen(
 ) {
   // State declarations
   const [content, setContent] = useState([]);
+  const [date, setDate] = useState("");
+  const [exercise, setExercise] = useState("");
+  const [duration, setDuration] = useState("");
+  const [distance, setDistance] = useState("");
+  const [notes, setNotes] = useState("");
+
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -39,9 +45,29 @@ function HomeScreen(
   //   setFormInput(e.target.value);
   // }
 
+  function handleDate(e) {
+    setDate(e.target.value);
+  }
+
+  function handleExercise(e) {
+    setExercise(e.target.value);
+  }
+
+  function handleDuration(e) {
+    setDuration(e.target.value);
+  }
+
+  function handleDistance(e) {
+    setDistance(e.target.value);
+  }
+
+  function handleNotes(e) {
+    setNotes(e.target.value);
+  }
+
   async function submitSession(e) {
     e.preventDefault();
-    console.log("Submitted session");
+    console.log(date, exercise, duration, distance, notes);
   }
 
   // Render
@@ -71,28 +97,56 @@ function HomeScreen(
       <form onSubmit={submitSession}>
         <h2>Add Session</h2>
         <div className="formLabelInput">
-          <label htmlFor="">Date:</label>
-          <input type="text" />
+          <label htmlFor="dateInput">Date:</label>
+          <input
+            id="dateInput"
+            type="date"
+            value={date}
+            onChange={handleDate}
+          />
         </div>
 
         <div className="formLabelInput">
-          <label htmlFor="">Exercise:</label>
-          <input type="text" />
+          <label htmlFor="exerciseSelect">Exercise:</label>
+          <input
+            id="exerciseSelect"
+            type="text"
+            value={exercise}
+            onChange={handleExercise}
+          />
         </div>
 
         <div className="formLabelInput">
-          <label htmlFor="">Duration:</label>
-          <input type="text" />
+          <label htmlFor="durationInput">Duration (Min):</label>
+          <input
+            id="durationInput"
+            type="number"
+            min="0"
+            value={duration}
+            onChange={handleDuration}
+          />
         </div>
 
         <div className="formLabelInput">
-          <label htmlFor="">Distance:</label>
-          <input type="text" />
+          <label htmlFor="distanceInput">Distance (K):</label>
+          <input
+            id="distanceInput"
+            type="number"
+            min="0"
+            step="0.1"
+            value={distance}
+            onChange={handleDistance}
+          />
         </div>
 
         <div className="formLabelInput">
-          <label htmlFor="">Notes:</label>
-          <input type="text" />
+          <label htmlFor="notesInput">Notes:</label>
+          <input
+            id="notesInput"
+            type="text"
+            value={notes}
+            onChange={handleNotes}
+          />
         </div>
         <button>Submit</button>
       </form>
