ce216a79cac2fc880582b144f5ae4073aa00e19f
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-12-02 17:10:55 -0500

    Import protected route component template

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import { Routes, Route, Link, useNavigate, useParams } from "react-router";
+
+// import apiSource
+import { apiSource } from "../apiSource";
+
+function ProtectedRoute({
+  // Props
+  children,
+}) {
+  // State declarations
+  const [verifying, setVerifying] = useState(true);
+  const [error, setError] = useState(null);
+
+  // Functions
+  const navigate = useNavigate();
+
+  // Check that credentials are valid, redirect if fail
+  useEffect(() => {
+    fetch(apiSource + "user/check/", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: localStorage.getItem("token"),
+      },
+    })
+      .then((response) => {
+        if (response.status == 401) {
+          localStorage.clear();
+          navigate("/login");
+        }
+        return response.json();
+      })
+      .catch((error) => setError(error))
+      .finally(setVerifying(false));
+  }, [children]);
+
+  // Render
+  if (verifying) return <p className="verifyDiv">Verifying...</p>;
+  if (error) return <p>Network error, please try again later.</p>;
+  
+  // Immediately redirect if no credentials at all
+  if (!localStorage.getItem("token") || !localStorage.getItem("isConfirmed")) {
+    return navigate("/login");
+  }
+
+  return children;
+}
+
+export default ProtectedRoute;
